d6fcc6f551122872cafed8b5380e7b7151e7f25a
rystei <gustavo208cardoso@gmail.com>
2026-09-21 20:58:34 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Musica: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano plus+");
+        }
+    }
+
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "AC";
+musica1.artista = "Telles";
+musica1.duracao = 186;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+
+musica2.nome = "Black";
+musica2.artista = "Rystei";
+musica2.duracao = 300;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
