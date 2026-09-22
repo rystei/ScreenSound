93db11e0086d760bee5bf4861695155e97eacf14
rystei <gustavo208cardoso@gmail.com>
2026-09-22 14:30:19 -0300

    Ex3: Desenvolver um método da classe Conta que exibe suas informações.

--- exercicios/Ex2.cs
+++ exercicios/Ex2.cs
@@ -4,4 +4,11 @@
     public int id;
     public double saldo;
     public int senha;
+
+   public void ExibirConta()
+    {
+        Console.WriteLine("INFORMAÇÕES DA CONTA:");
+        Console.WriteLine($"Titular: {titular}");
+        Console.WriteLine($"Saldo atual: {saldo}");
+    }
 }
--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -4,6 +4,4 @@ conta.id = 1;
 conta.saldo = 10000050;
 conta.senha = 1234;
 
-Console.WriteLine("INFORMAÇÕES DA CONTA:");
-Console.WriteLine($"Titular: {conta.titular}");
-Console.WriteLine($"Saldo atual: {conta.saldo}");
+conta.ExibirConta();
